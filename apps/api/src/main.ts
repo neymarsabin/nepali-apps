27e30b32ac3b7e0481ad3e0bf3b508d69e643983
Prabesh Dahal <dprabesh11@gmail.com>
2024-04-13 13:16:52 +0545

    Add API for JSON data (#9)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DataModule } from './data.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(DataModule);
   await app.listen(8000);
 }
 bootstrap();
